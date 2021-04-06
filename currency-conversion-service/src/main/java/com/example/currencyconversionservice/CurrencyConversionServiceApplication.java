bfd9b2a11ac38f1012c9adc62a84aab20bc437b0
Vikash Kumar <44017766+vkrvikash@users.noreply.github.com>
2021-04-06 10:04:44 +0530

    Add files via upload

---
+++ currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -0,0 +1,18 @@
+package com.example.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients("com.example.currencyconversionservice")
+@EnableDiscoveryClient
+//pass package to be scanned
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+
+}
